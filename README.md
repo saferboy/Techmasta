acf812a75262dc686d8f517b7ea831d31d4abaed
saferboy <muxtordev@gmail.com>
2025-02-06 12:02:17 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# DYHXX
+
+---
+
+### Docker Commands
+
+1. Run Docker for **dev** mode:
+```bash
+docker compose -f docker-compose.dev.yml up --build -d
+```
+2. Shutdown Docker compose
+```bash
+docker compose -f docker-compose.dev.yml down
+```
+### dev
+
+3.1 Run Docker for test mode:
+```bash
+docker-compose -f docker-compose.dev2.yml up
+```
+
+3.2 Shutdown Docker Compose
+```bash
+docker-compose -f docker-compose.dev2.yml down
+```
---
+++ backend/src/main.ts
@@ -0,0 +1,42 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+// import * as cookieParser from 'cookie-parser';
+// import { PrismaClientExceptionFilter } from "./common/helpers/prisma-exception";
+// import { BigIntInterceptor } from "./common/helpers/big-int.interceptor";
+import { config } from 'dotenv';
+
+config();
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setGlobalPrefix('api/v1');
+  const isProd = process.env.NODE_ENV === 'production';
+  // app.use(cookieParser());
+  app.enableCors({
+    credentials: true,
+    origin: isProd ? 'https://techmasta.uz' : true,
+  });
+  // app.useGlobalFilters(new PrismaClientExceptionFilter());
+  // app.useGlobalInterceptors(new BigIntInterceptor());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  const port = 3022;
+  await app.listen(+port);
+  return port;
+}
+
+bootstrap()
+  .then((port) => {
+    Logger.log('Server is running on host: http://localhost:' + port);
+  })
+  .catch((e) => {
+    console.log(e);
+    Logger.error('Server rejected');
+  });

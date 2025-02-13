3cca8a55afd5b05f97d088f6dadfbded34b055e6
Alisher1221-ru <kr4054658@gmail.com>
2025-02-13 11:07:45 +0500

    fix: added cliNest

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
-// import * as cookieParser from 'cookie-parser';
-// import { PrismaClientExceptionFilter } from "./common/helpers/prisma-exception";
-// import { BigIntInterceptor } from "./common/helpers/big-int.interceptor";
 import { config } from 'dotenv';
 
 config();
@@ -13,13 +10,10 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('api/v1');
   const isProd = process.env.NODE_ENV === 'production';
-  // app.use(cookieParser());
   app.enableCors({
     credentials: true,
     origin: isProd ? 'https://techmasta.uz' : true,
   });
-  // app.useGlobalFilters(new PrismaClientExceptionFilter());
-  // app.useGlobalInterceptors(new BigIntInterceptor());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -27,14 +21,14 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  const port = 3022;
+  const port = 3030;
   await app.listen(+port);
   return port;
 }
 
 bootstrap()
   .then((port) => {
-    Logger.log('Server is running on host: http://localhost:' + port);
+    Logger.log(`Server is running on host: http://localhost:${port}`);
     console.log(`Server running on port ${port}`);
   })
   .catch((e) => {
